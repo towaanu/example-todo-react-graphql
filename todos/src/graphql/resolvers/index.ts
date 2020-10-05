d049bbd7f7cbca3b2ce1c87a93ee143505ff40da
Antoine Muller <niantoinesan@gmail.com>
2020-10-05 09:04:29 +0200

    add apollo server

---
+++ todos/src/graphql/resolvers/index.ts
@@ -0,0 +1,7 @@
+const resolvers = {
+    Query: {
+        hello: () => "World",
+    },
+};
+
+export default resolvers
--- todos/src/main.ts
+++ todos/src/main.ts
@@ -1,1 +1,10 @@
-console.log("Hello todos !")
+import { ApolloServer } from 'apollo-server';
+import { typeDefs, resolvers } from './graphql';
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen({ port: 8050 })
+    .then(({ url }) => {
+        console.log(`Server running at ${url}`);
+    })
+    .catch(err => console.error(err));
