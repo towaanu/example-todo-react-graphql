f9f07d769709abdece61068b379321e76b45b5bb
Antoine Muller <niantoinesan@gmail.com>
2020-10-06 12:04:26 +0200

    add docker-compose for running client and server

---
+++ docker_configs/server/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+DB_USER=postgres
+DB_HOST=postgres_db
+
+# wait until Postgres is ready
+while ! pg_isready -q -h $DB_HOST -p 5432 -U $DB_USER
+do
+  echo "$(date) - waiting for database to start"
+  sleep 2
+done
+
+# typeorm migration:run
+./node_modules/.bin/typeorm migration:run
+node dist/main.js
+
+# bin="/app/bin/kaimono"
+# eval "$bin eval \"Kaimono.Release.migrate\""
+# # start the elixir application
+# exec "$bin" "start"
